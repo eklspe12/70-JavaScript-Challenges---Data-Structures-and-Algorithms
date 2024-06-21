c81267d3536f7d7d1f1d44bd380964f20191fd24
Spencer <eklund.spencer@outlook.com>
2024-06-21 16:21:45 -0600

    solved by comparing variable to next number in array

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(array) {
+	let max = array[0];
+	for (i = 0; i < array.length; i++) {
+		if (array[i] > max) {
+			max = array[i];
+		}
+	}
+	return max;
+}
 
 module.exports = findMaxNumber;
