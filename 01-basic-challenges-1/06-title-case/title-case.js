805bff163432c536f513b1544e0b0e5f97626e78
Spencer <eklund.spencer@outlook.com>
2024-06-25 15:20:37 -0600

    solved by splitting string then rejoining

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,17 @@
-function titleCase() {}
+function titleCase(str) {
+	let words = str.split(' ');
+	let capitalWords = words.map((word) => {
+		return word.charAt(0).toUpperCase() + word.slice(1);
+	});
+	let newString = capitalWords.join(' ');
+	return newString;
+}
+
+// function titleCase(str) {
+//     seperate strings into a set or Array
+//     modify each new string to be a capital
+//     put array or set of strings back together
+//     return new string
+// }
 
 module.exports = titleCase;
