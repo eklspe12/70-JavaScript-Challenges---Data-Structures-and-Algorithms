12f7a913f7ee55ce05fc20019d331db7ba881fb1
Spencer <eklund.spencer@outlook.com>
2024-06-25 16:03:30 -0600

    solved by removing special characters and capital letters of string, seperating into an array to be interated backwards with for loop, joined new array, then returned the comparison between both strings

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,14 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+	let forward = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+	let forwardArray = forward.split('');
+	let backwardArray = [];
+	for (i = forwardArray.length - 1; i >= 0; i--) {
+		backwardArray.push(forwardArray[i]);
+	}
+	let backward = backwardArray.join('');
+	return backward === forward;
+}
+
+isPalindrome('A man, a plan, a canal, Panama');
 
 module.exports = isPalindrome;
