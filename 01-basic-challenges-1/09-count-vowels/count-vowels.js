7247257ccb6855878ad4f82c152c792379bb737a
Spencer <eklund.spencer@outlook.com>
2024-06-25 16:14:17 -0600

    solved by splitting string into array, then checking to see if each letter in array was a vowel and adding to count if true, then returned count

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,18 @@
-function countVowels() {}
+function countVowels(str) {
+	let stringArray = str.split('');
+	let count = 0;
+	for (i = 0; i < stringArray.length; i++) {
+		if (
+			stringArray[i].toLowerCase() === 'a' ||
+			stringArray[i].toLowerCase() === 'e' ||
+			stringArray[i].toLowerCase() === 'i' ||
+			stringArray[i].toLowerCase() === 'o' ||
+			stringArray[i].toLowerCase() === 'u'
+		) {
+			count++;
+		}
+	}
+	return count;
+}
 
 module.exports = countVowels;
