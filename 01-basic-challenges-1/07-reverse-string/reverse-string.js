9a1a7fbcd7242dc5f5d3015407b8a03f6584fb79
Spencer <eklund.spencer@outlook.com>
2024-06-25 15:38:44 -0600

    solved by splitting letters into array, then using a for loop to add each separate letter to an empty array, then joined and returned the array

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,18 @@
-function reverseString() {}
+function reverseString(str) {
+	let letters = str.split('');
+	let reverseArray = [];
+	for (i = letters.length - 1; i >= 0; i--) {
+		reverseArray.push(letters[i]);
+	}
+	let reverseString = reverseArray.join('');
+	return reverseString;
+}
+
+reverseString('hello');
+
+// function takes in a string
+//      create array to hold new variable of split string
+//      for each letter of array, add to new array starting at end
+//      return new array
 
 module.exports = reverseString;
