88e71d2f34447137bf2544c9f848085c80d6282b
Spencer <eklund.spencer@outlook.com>
2024-06-25 16:21:27 -0600

    solved by converting array to a set then back to an array

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,8 @@
-function removeDuplicates() {}
+function removeDuplicates(array) {
+	let set = new Set(array);
+	let noDuplicates = Array.from(set);
+	return noDuplicates;
+}
 
+removeDuplicates([1, 3, 4, 6, 3, 2, 1]);
 module.exports = removeDuplicates;
