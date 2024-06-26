f6db7fe90c3a07d18146d1ae2198ad8296501496
Spencer <eklund.spencer@outlook.com>
2024-06-26 11:18:20 -0600

    solved by created new array of sorted numbers, iterated the length of the array, returned i value not included in sorted array

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,19 @@
-function findMissingNumber() {}
+function findMissingNumber(array) {
+	sortedArray = array.sort();
+	for (i = sortedArray[0]; i <= sortedArray.length; i++) {
+		if (!sortedArray.includes(i)) {
+			console.log(i);
+			return i;
+		}
+	}
+}
+
+findMissingNumber([2, 5, 3, 1]);
+
+// function takes in array of numbers
+//      sortedArray hold numbers in ascending order
+//      for loop iterates each member to see if it is equal to i
+//          sets varaible to equal number not equal to i
+// return variable of missing number
 
 module.exports = findMissingNumber;
