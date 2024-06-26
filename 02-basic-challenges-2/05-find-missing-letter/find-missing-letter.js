311da607c02f10470403d694496d756de74df81e
Spencer <eklund.spencer@outlook.com>
2024-06-26 11:51:00 -0600

    created variable of all alphabet characters, created variable to hold index of first letter in array, ran a for loop for the length of the array that checked if the alphabet character at the position of i + index was the same as the character in the next position of array, returned character if false

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,21 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+	const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	const index = alphabet.indexOf(arr[0]);
+	for (i = 0; i < arr.length; i++) {
+		if (alphabet.charAt(index + i) !== arr[i]) {
+			return alphabet.charAt(index + i);
+		}
+	}
+}
+
+findMissingLetter(['a', 'b', 'c', 'e']);
+findMissingLetter(['X', 'Z']);
+findMissingLetter(['m', 'n', 'o', 'q', 'r']);
+findMissingLetter(['F', 'G', 'H', 'J']);
+// const to hold all char of alphabet
+// const to hold first elements value
+// iterate through array for the length of array
+//      check if value of position is not the same as i plus position
+//          if not the same, return what is present at i position
 
 module.exports = findMissingLetter;
