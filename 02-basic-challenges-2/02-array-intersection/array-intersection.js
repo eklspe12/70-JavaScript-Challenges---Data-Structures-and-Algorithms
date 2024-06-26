0c2c543f7e4f1d46425a10e974d4e54926a5e63a
Spencer <eklund.spencer@outlook.com>
2024-06-26 10:42:51 -0600

    function takes in two arrays, creates a new empty array, loops through each element of arr1 to see if arr2 includes it, adds it to duplicates if true, returns array

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,22 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+	let duplicateArray = [];
+	for (i = 0; i < arr1.length; i++) {
+		if (arr2.includes(arr1[i])) {
+			duplicateArray.push(arr1[i]);
+		}
+	}
+	return duplicateArray;
+}
+
+arrayIntersection([1, 2, 3, 4], [1, 3, 5, 6]);
+arrayIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
+arrayIntersection([10, 20, 30], [30, 40, 50]);
+arrayIntersection([1, 2, 3], [4, 5, 6]);
+
+// function takes in two arrays
+//      new array is created to hold duplicates
+//      one array is looped through to check if any of the elements are included in other array
+//          any same elements are added to a new array
+//      new array is returned
 
 module.exports = arrayIntersection;
