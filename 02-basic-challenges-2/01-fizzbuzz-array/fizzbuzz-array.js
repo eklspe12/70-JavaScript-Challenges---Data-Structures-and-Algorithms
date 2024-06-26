4fe652cddfe6d88ad1f7d11245485247c1133690
Spencer <eklund.spencer@outlook.com>
2024-06-26 10:27:20 -0600

    created empty array, iterated from one to num passed into function, if else statements to sort what is pushed to array, array returned

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,25 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+	let fizzBuzzArray = [];
+	for (i = 1; i <= num; i++) {
+		if (Number.isInteger(i / 3) && Number.isInteger(i / 5)) {
+			fizzBuzzArray.push('FizzBuzz');
+		} else if (Number.isInteger(i / 3)) {
+			fizzBuzzArray.push('Fizz');
+		} else if (Number.isInteger(i / 5)) {
+			fizzBuzzArray.push('Buzz');
+		} else {
+			fizzBuzzArray.push(i);
+		}
+	}
+	console.log(fizzBuzzArray);
+	return fizzBuzzArray;
+}
+
+fizzBuzzArray(30);
+
+// create an empty array to hold number and word
+//      iterate through all numbers
+//          check if the number divided by 3 or 5 is a whole number
+//              add to array if yes
 
 module.exports = fizzBuzzArray;
