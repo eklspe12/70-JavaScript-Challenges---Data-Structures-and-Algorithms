8619f689f546586593d6001f8355cf3e5ea46ec9
Spencer <eklund.spencer@outlook.com>
2024-06-26 18:18:59 -0600

    split str into arr then iterated through array comparing each element to immediate neighbors

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,17 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+	let strArr = str.split('');
+	for (i = 0; i < strArr.length; i++) {
+		if (strArr[i] !== strArr[i + 1] && strArr[i] !== strArr[i - 1]) {
+			return strArr[i];
+		}
+	}
+	return null;
+}
+
+// function takes in a str
+//      str is split into array
+//          iterate through array and compare index location to immediate neighbors
+//              return element if no match
+//      return null if all matches
 
 module.exports = findFirstNonRepeatingCharacter;
