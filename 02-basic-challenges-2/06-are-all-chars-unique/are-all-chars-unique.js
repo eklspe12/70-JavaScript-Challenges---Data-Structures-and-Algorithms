4e53f84d711a610d8246a967f0ce6811b985c23c
Spencer <eklund.spencer@outlook.com>
2024-06-26 18:07:45 -0600

    solved by splitting str into an arr, initializing a new arr to hold uniques, then iterating through str arr to check see if uniques has value

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,23 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+	let strArr = str.split('');
+	let uniques = [];
+	for (i = 0; i < strArr.length; i++) {
+		if (!uniques.includes(strArr[i])) {
+			uniques.push(strArr[i]);
+		} else {
+			return false;
+		}
+	}
+	return true;
+}
+
+// funciton takes in a str
+//      str is split so all characters are elements
+//      create array for uniques
+//      str array is iterated over
+//          if unique arr doesn't contain element add it
+//              else return false
+//          return true
+//
 
 module.exports = areAllCharactersUnique;
