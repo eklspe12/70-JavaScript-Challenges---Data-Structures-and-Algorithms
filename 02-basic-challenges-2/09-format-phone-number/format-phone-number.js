4609c31bdf2b364527d25d3f01771e798d769a46
Spencer <eklund.spencer@outlook.com>
2024-06-26 18:50:19 -0600

    created an interloped string to hold correct format

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,10 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+	let formattedNumber = `(${arr[0]}${arr[1]}${arr[2]}) ${arr[3]}${arr[4]}${arr[5]}-${arr[6]}${arr[7]}${arr[8]}${arr[9]}`;
+	return formattedNumber;
+}
+
+// funciton takes in array of numbers
+//      create var to hold arr as string
+//      return var
 
 module.exports = formatPhoneNumber;
