fb265b9cf5f923805ad34cb701d4eec242633577
Spencer <eklund.spencer@outlook.com>
2024-07-01 10:43:53 -0600

    created array to hold string split into words, a var to hold the alphabet, and a var to hold the current leader word and score; then iterated over each unique word- splitting each word then finding the index value of the current char, added the value to a total, then compared the total to the leader

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,28 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+	let wordsArr = str.split(' ');
+	let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	let leader = { word: '', score: 0 };
+	for (i = 0; i < wordsArr.length; i++) {
+		let total = 0;
+		wordsArr[i].split('').forEach((char) => {
+			total = total + alphabet.indexOf(char);
+		});
+		if (total > leader.score) {
+			leader.word = wordsArr[i];
+			leader.score = total;
+		}
+	}
+	return leader.word;
+}
+
+highestScoringWord('my name is spencer');
+
+//function takes in a string
+//      new array is made by splitting the words of str
+//      var that holds alphabet as a string to be indexed
+//      for loop for each member of array
+//          each letter of current string is indexed against var and number added to count
+//              count compared to current leader to see if it is replaced
+//      current leader is returned
 
 module.exports = highestScoringWord;
