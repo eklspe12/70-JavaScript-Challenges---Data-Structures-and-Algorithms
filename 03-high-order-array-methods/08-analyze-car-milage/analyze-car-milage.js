6dc4f2292378d4c1e5c920557924ab6dcd203252
Spencer <eklund.spencer@outlook.com>
2024-07-01 17:09:57 -0600

    used forEach to compare values for highest and lowest mileage, used reduce to get total, then divided total mileage by arr.length

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,22 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(arr) {
+	let highestMileageCar = { make: '', model: '', year: 0, mileage: 0 };
+	let lowestMileageCar = { make: '', model: '', year: 0, mileage: 1000000 };
 
+	arr.forEach((car) => {
+		if (car.mileage < lowestMileageCar.mileage) {
+			lowestMileageCar = car;
+		}
+		if (car.mileage > highestMileageCar.mileage) {
+			highestMileageCar = car;
+		}
+	});
+	let totalMileage = arr.reduce((sum, car) => {
+		return sum + car.mileage;
+	}, 0);
+	let averageMileage = totalMileage / arr.length;
+	return { averageMileage, highestMileageCar, lowestMileageCar, totalMileage };
+}
+
+// function takes in an array of objects
+//
 module.exports = analyzeCarMileage;
