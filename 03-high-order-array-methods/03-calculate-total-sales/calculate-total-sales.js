b6c035bf71f19cce3d8c5a894e7580a765803a10
Spencer <eklund.spencer@outlook.com>
2024-07-01 10:19:42 -0600

    solved by mapping products array to calculate price * quantity * tax, then returned the new array reduced

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,22 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, tax) {
+	let sumArray = products.map((p) => {
+		return p.price * p.quantity * (1 + tax / 100);
+	});
+	return sumArray.reduce((total, sum) => {
+		return total + sum;
+	}, 0);
+}
 
+calculateTotalSalesWithTax(
+	[
+		{ name: 'Apple', price: 0.5, quantity: 10 },
+		{ name: 'Banana', price: 0.3, quantity: 20 },
+		{ name: 'Orange', price: 0.6, quantity: 15 },
+	],
+	8
+);
+
+// function takes in products and taxrate
+//      create const to hold new array of forEach with the quantity multiplied by the cost
+//      add all together and multiply by tax rate
 module.exports = calculateTotalSalesWithTax;
