42bbfcfde1be96ca3bbd14419a9018094e62f424
Spencer <eklund.spencer@outlook.com>
2024-07-01 09:28:37 -0600

    solved by using if statements to verify inclusion of @, ., and that @ was not first index

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,12 @@
-function validateEmail() {}
+function validateEmail(str) {
+	if (str.includes('.') && str.includes('@') && str.indexOf('@') !== 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+validateEmail('eso@outlook.com');
+validateEmail('@outlook.com');
 
 module.exports = validateEmail;
