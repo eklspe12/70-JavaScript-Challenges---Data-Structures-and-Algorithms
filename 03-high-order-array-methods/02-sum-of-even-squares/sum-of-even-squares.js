58d78e8954dd674c5e091021c45edb18f55f40a2
Spencer <eklund.spencer@outlook.com>
2024-07-01 09:51:35 -0600

    used filter to remove any number not cleanly divisble by 2, then used reduce to square and sum remaining numbers

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,14 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+	let evenNums = arr.filter((num) => num % 2 === 0);
+	let answer = evenNums.reduce((total, num) => {
+		return total + num * num;
+	}, 0);
+	console.log(answer);
+	return answer;
+}
+
+//function takes in array
+//      filter through to get all even numbers
+//      use reduce to square and add all remaining numbers
 
 module.exports = sumOfEvenSquares;
