570b9848dc6a449d0a20c7375de8f17e5da8b931
Spencer <eklund.spencer@outlook.com>
2024-07-01 10:58:38 -0600

    solved by cresting varaibles to hold each string split into an array, sorted, then rejoined to be compared against each other for equality

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,18 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+	let str1Var = str1.split('').sort().join();
+	let str2Var = str2.split('').sort().join();
+	if (str1Var === str2Var) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+validAnagrams('hello', 'goodbye');
+
+//function takes in 2 strings
+//      each string is separated into arrays of characters
+//      sort each array alphabetically
+//      compare the arrays rejoined into a string
 
 module.exports = validAnagrams;
