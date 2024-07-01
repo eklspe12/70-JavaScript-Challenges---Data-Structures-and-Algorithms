dad846351b30c5a28f31e3b389f8d984bb90c8c6
Spencer <eklund.spencer@outlook.com>
2024-07-01 16:34:12 -0600

    split string into array, checked that the array had only 4 elements, checked if every element was between 0-255 and equal to itself when converted to an integer

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,19 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+	let numArr = str.split('.');
+	if (numArr.length !== 4) {
+		return false;
+	}
+	return numArr.every((num) => {
+		let number = parseInt(num);
+		return number >= 0 && number <= 255 && number.toString() === num;
+	});
+};
+
+isValidIPv4('12.12.12.12');
+
+//function takes in a string
+//      string is split into an array by . location
+//          each str in array is checked to see if it's between 1-255
+//          true or false based on result
 
 module.exports = isValidIPv4;
