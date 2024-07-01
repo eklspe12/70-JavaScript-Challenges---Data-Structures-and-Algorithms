776a8dc7ef31becbe1dd93c0685b3839b8723b2a
Spencer <eklund.spencer@outlook.com>
2024-07-01 09:38:56 -0600

    exercise completed

--- 03-high-order-array-methods/01-simple-examples/simple-examples.js
+++ 03-high-order-array-methods/01-simple-examples/simple-examples.js
@@ -3,39 +3,41 @@ const numbers = [1, 2, 3, 4, 5];
 /**
  * map: Transforms array elements with a provided function, creating a new array.
  */
-
+const mapNumbers = numbers.map((num) => {
+	num * 2;
+});
 
 /**
  * filter: Creates a new array with elements that satisfy a specified condition.
  */
-
-
+const filteredArray = numbers.filter((num) => {
+	return num < 3;
+});
 
 /**
  * reduce: Accumulates array elements into a single value using a provided function.
  */
-
-
-
+const reducedArray = numbers.recude((total, num) => {
+	return total + num;
+}, 0);
 /**
  * forEach: Iterates through array elements and applies a function without creating a new array.
  */
+const forEachArray = numbers.forEach((num) => {
+	console.log(num);
+});
 
-
-
- /**
+/**
  * find: Returns the first array element that satisfies a specified condition.
  */
-
+const find = numbers.find((num) => num > 4);
 
 /**
  * some: Checks if at least one array element satisfies a condition.
  */
-
- 
+const some = numbers.some((num) => num < 1);
 
 /**
  * every: Checks if all array elements satisfy a condition.
  */
-
-
+const every = numbers.every((num) => num < 0);
