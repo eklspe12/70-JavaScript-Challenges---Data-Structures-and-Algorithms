bd9840482718f131002ce20297805ae3ebc813c4
Spencer <eklund.spencer@outlook.com>
2024-08-06 10:50:06 -0600

    set base case to str length equals zero and returned empty string when true, then created a var to store the last char and a var to store the str without the last char, finally returned lastChar + the result of the newStr called into the funciton recursively

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,17 @@
-function reverseString() {}
+function reverseString(str) {
+	if (str.length === 0) {
+		return '';
+	}
+	let lastChar = str.slice(-1);
+	let newStr = str.slice(0, -1);
+	return lastChar + reverseString(newStr);
+}
+
+reverseString('Hello');
 
 module.exports = reverseString;
+
+// function takes in a str
+//      base case is str is empty or length = 1
+//      recursively add remove letter to an array/str
+//      return reverse str
