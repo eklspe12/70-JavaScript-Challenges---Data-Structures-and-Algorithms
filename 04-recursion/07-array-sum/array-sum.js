354b66752b9b02d14de673076615b18ce2bb25c8
Spencer <eklund.spencer@outlook.com>
2024-08-06 11:37:00 -0600

    set base cases for if arr.length equals 1 or 0, returning arr[0] or 0 respectively. Then returned array[0] + recursive function with array.slice(1) passed in

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,17 @@
-function arraySum() {}
+function arraySum(array) {
+	if (array.length === 1) {
+		return array[0];
+	} else if (array.length <= 0) {
+		return 0;
+	}
+	return array[0] + arraySum(array.slice(1));
+}
+
+console.log(arraySum([-1, -2, -3, -4, -5]));
 
 module.exports = arraySum;
+
+//function takes in an array
+//  base case is when array length equals 1, returns array element
+//  second base case when arr length equals 0, return 0
+// return array element 1 + recursive function with array slice(1) passed in
