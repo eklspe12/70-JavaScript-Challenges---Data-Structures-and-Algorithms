6c022c31d8846ffb0d987c0c3152551dd1619762
Spencer <eklund.spencer@outlook.com>
2024-08-06 10:19:12 -0600

    created a base case of num equals zero, if num didn't equal zero, decremented num and called function recursively

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,13 @@
-function countDown() {}
+function countDown(num) {
+	if (num <= 0) {
+		console.log('All done!');
+		return;
+	} else {
+		console.log(num);
+		num--;
+		countDown(num);
+	}
+}
 
+countDown(7);
 module.exports = countDown;
