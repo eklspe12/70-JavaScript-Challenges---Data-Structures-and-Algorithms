a3ce71197e8dc703f88a92dc09ef04344ba96c3f
Spencer <eklund.spencer@outlook.com>
2024-08-06 11:26:50 -0600

    set base case if exp equals 1 return base or if exp equals 0 return 1, then returned base multiplied by function with exp -1

--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,15 @@
-function power() {}
+function power(base, exponent) {
+	if (exponent === 1) {
+		return base;
+	} else if (exponent <= 0) {
+		return 1;
+	}
+	return base * power(base, exponent - 1);
+}
 
 module.exports = power;
+
+console.log(power(5, 3));
+//function take in base and exponent
+//  base case checks if exponent is equal to 1 or 0, returning base or 1 respectively
+//  return base * recursive funciton with exponent -1
