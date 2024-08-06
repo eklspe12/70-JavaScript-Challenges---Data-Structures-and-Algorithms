c2cb4695fbf9ef09a5cdeb372ea91d51c2081a87
Spencer <eklund.spencer@outlook.com>
2024-08-06 11:55:19 -0600

    base case checks if start and end numbers are the same, returning start number as an array if true. numbers var is declared equal to numberRange fucntion with endNum -1 passed in, the end number is then pushed into var when recursion has ended and numbers var is passed back up until complete

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,15 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+	if (startNum === endNum) {
+		return [startNum];
+	}
+	let numbers = numberRange(startNum, endNum - 1);
+	numbers.push(endNum);
+	return numbers;
+}
 
+console.log(numberRange(1, 5));
 module.exports = numberRange;
+
+//function takes in takes in startNum and endNum
+//      Base case checks if startNum and endNum are equal, return end Num if true
+//      returns array[startNum] + recursive funciton with startNum + 1 passed in
