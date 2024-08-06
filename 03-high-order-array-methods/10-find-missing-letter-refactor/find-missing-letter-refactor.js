190fb394dfc10524850fbe6a4c62cc1d8ac6da56
Spencer <eklund.spencer@outlook.com>
2024-08-06 10:11:00 -0600

    created variable to store first char unicode, then iterated through array to check if each char matched the start variable plus i, returned the missing unicode value converted back into a char string

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,15 @@
-function findMissingLetter() {}
+function findMissingLetter(array) {
+	let start = array[0].charCodeAt(0);
+	for (i = 0; i < array.length; i++) {
+		if (start + i !== array[i].charCodeAt(0)) {
+			return String.fromCharCode(start + i);
+		}
+	}
+}
 
+console.log(findMissingLetter(['a', 'b', 'c', 'e']));
 module.exports = findMissingLetter;
+
+//function takes in an array
+//  store the unicode value of first array position
+//
