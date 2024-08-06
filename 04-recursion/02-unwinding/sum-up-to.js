da305ae11bf373206e71dabe4ed694e9f69a5912
Spencer <eklund.spencer@outlook.com>
2024-08-06 10:25:59 -0600

    called function recursively subtracting 1 from num until base case of num equals 1, then added up the stack

--- 04-recursion/02-unwinding/sum-up-to.js
+++ 04-recursion/02-unwinding/sum-up-to.js
@@ -1,3 +1,12 @@
-function sumUpTo() {}
+function sumUpTo(num) {
+	if (num === 1) {
+		return 1;
+	}
+	return num + sumUpTo(num - 1);
+}
+
+console.log(sumUpTo(4));
 
 module.exports = sumUpTo;
+
+//
