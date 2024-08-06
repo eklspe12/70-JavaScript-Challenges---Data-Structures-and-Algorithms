792c47bd33d92c11097547a34d16616ddf1a9c2d
Spencer <eklund.spencer@outlook.com>
2024-08-06 11:18:08 -0600

    base case first checks if number is equal to 1 or 0 and returns 1 if true, then function returns num multiplied by the recursive function taking in num -1

--- 04-recursion/05-factorial/factorial.js
+++ 04-recursion/05-factorial/factorial.js
@@ -1,3 +1,8 @@
-function factorial() {}
-
+function factorial(num) {
+	if (num <= 0 || num === 1) {
+		return 1;
+	}
+	return num * factorial(num - 1);
+}
+console.log(factorial(0));
 module.exports = factorial;
