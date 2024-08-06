d4561e2d633fc9f7f53fc85c3a979634504a07fc
Spencer <eklund.spencer@outlook.com>
2024-08-06 11:08:01 -0600

    set two base cases, one for num equals zero and one for num equals 1, if not base case return recursive funciton of num -1 and recursive funciton of num -2 since fib wants the sum of previous two numbers

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,16 @@
-function fibonacci() {}
+function fibonacci(num) {
+	if (num <= 0) {
+		return num;
+	} else if (num === 1) {
+		return num;
+	}
+	return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+console.log(fibonacci(7));
 
 module.exports = fibonacci;
+
+//function takes in a number
+//      set base case for if num is equal to 1 and a base case for zero
+//      if not base case return fib of num minus 1 plus fib of num -2
