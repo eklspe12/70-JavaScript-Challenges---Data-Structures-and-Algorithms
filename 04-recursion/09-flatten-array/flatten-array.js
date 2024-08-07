060bc3754bf8b63a9233e7e1c0118d4f3afe9975
Spencer <eklund.spencer@outlook.com>
2024-08-07 08:49:00 -0600

    first created empty array to hold elements ,then iterated over original arr to check if it's elements were arrays themselves, which would recursively call the funciton until it was broken into an item, pushes items into results var before returning

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,20 @@
-function flattenArray() {}
+function flattenArray(arr) {
+	let result = [];
+	for (const item of arr) {
+		if (Array.isArray(item)) {
+			result = result.concat(flattenArray(item));
+		} else {
+			result.push(item);
+		}
+	}
+	return result;
+}
 
 module.exports = flattenArray;
+
+// function takes in array of arrays to be combines into one array
+//      declare result variable to be new array
+//      iterate over original array to check if each element is an array or not
+//      if an element is an array, recursively run the function until it isn't
+//      otherwise push the item into the array
+// return result array
