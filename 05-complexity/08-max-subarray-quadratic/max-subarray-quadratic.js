5c908bb25a115186b14f3786a9ab7b98fce66895
Spencer <eklund.spencer@outlook.com>
2024-09-18 13:57:35 -0600

    solved with a nested for loop that added three consecutive numbers of array then compared to highest current sum

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,25 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+	let maxSum = 0;
+	for (i = 0; i <= arr.length - k; i++) {
+		let tempSum = 0;
+		for (j = i; j < i + k; j++) {
+			tempSum = tempSum + arr[j];
+		}
+		if (tempSum > maxSum) {
+			maxSum = tempSum;
+		}
+	}
+	console.log(maxSum);
+}
+
+maxSubarraySum([1, 30, 5, 7, 8, 4, 200], 3);
 
 module.exports = maxSubarraySum;
+
+// declare function that takes in array and integer as arguments
+//      declare varibale to hold highest sum
+//      loop through each item of array
+//              create temp sum
+//              loop again = to k to add next numbers to tempsum
+//              maxsum = tempsum if temp > max
+// return max
