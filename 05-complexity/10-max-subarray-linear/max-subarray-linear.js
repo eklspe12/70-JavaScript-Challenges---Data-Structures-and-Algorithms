52d3e335295d7d9fa2dc5c1c475c449df2b7dbfb
Spencer <eklund.spencer@outlook.com>
2024-09-18 14:12:59 -0600

    solved by looping through each number of array and adding it with the next two numbers if possible, then comparing the sum to the max sum

--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,3 +1,15 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+	let max = -Infinity;
+	for (i = 0; i < arr.length - (k - 1); i++) {
+		let temp = arr[i] + arr[i + 1] + arr[i + 2];
+		if (temp > max) {
+			max = temp;
+		}
+	}
+	console.log(max);
+	return max;
+}
+
+maxSubarraySum([-1, -5, -40, -2, -1, -6, -400, -400], 3);
 
 module.exports = maxSubarraySum;
