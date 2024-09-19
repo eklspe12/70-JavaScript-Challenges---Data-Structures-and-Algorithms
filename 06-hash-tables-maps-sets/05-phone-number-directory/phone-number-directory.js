f11ab7db39503447965e814497d759eb000818bf
Spencer <eklund.spencer@outlook.com>
2024-09-19 14:43:50 -0600

    solved by creating an array to hold contact info split without colon, then created a map to store array elements as keys and values

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,26 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(arr) {
+	let contacts = [];
+	for (const name of arr) {
+		contacts.push(name.split(':'));
+	}
+	let contactMap = new Map();
+	for (const person of contacts) {
+		contactMap.set(person[0], person[1]);
+	}
+	return contactMap;
+}
 
+console.log(
+	phoneNumberDirectory([
+		'John:123-456-7890',
+		'Jane:987-654-3210',
+		'Joe:555-555-5555',
+	])
+);
+
+// function takes in an array of names and phone numbers
+//      declare a new array that splits the names from the phone numbers
+//      declare a new map to hold data
+//      run a for loop of the new array to set each name and number in the new map
+//      return the new map
 module.exports = phoneNumberDirectory;
