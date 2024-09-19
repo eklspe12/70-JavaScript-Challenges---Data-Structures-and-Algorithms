4cd1b8b3caef5c4713199b583ad4f38fed78c240
Spencer <eklund.spencer@outlook.com>
2024-09-19 14:35:25 -0600

    solved by sanatizing string and splitting words into array, creating a new map to hold the words as keys with values, then running a for loop to see if the word was included in the map, adding to the value if it was"

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,26 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(string) {
+	let cleanStrings = string
+		.replace(/[^\w\s]/g, '')
+		.toLowerCase()
+		.split(' ');
+	const wordCount = new Map();
+	for (const word of cleanStrings) {
+		if (wordCount.has(word)) {
+			wordCount.set(word, wordCount.get(word) + 1);
+		} else {
+			wordCount.set(word, 1);
+		}
+	}
+	return wordCount;
+}
+
+console.log(wordFrequencyCounter('This is my string and this is the words.'));
 
 module.exports = wordFrequencyCounter;
+
+//  function takes in a senctence as a string
+//      variable is declared to store the instances of each word and their occurences
+//      string is converted to lower case without punctuation
+//      create new map to hold keys and values
+//      split the string and add each key value pair to an array
+//          for loop of array to iterate through each kery value, adding them to a map
