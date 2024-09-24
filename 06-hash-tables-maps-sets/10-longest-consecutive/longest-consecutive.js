697867c5b1ebe749434b2b206d63a6d018557141
Spencer <eklund.spencer@outlook.com>
2024-09-24 11:29:53 -0600

    explanation found in pseudocode

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,23 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(numbers) {
+	const numberSet = new Set(numbers);
+	let longest = 0;
+
+	for (const num of numberSet) {
+		if (!numberSet.has(num - 1)) {
+			let currentNum = num;
+			let currentLongest = 1;
+
+			while (numberSet.has(currentNum + 1)) {
+				currentLongest++;
+				currentNum++;
+			}
+			longest = Math.max(longest, currentLongest);
+		}
+	}
+	return longest;
+}
 
 module.exports = longestConsecutiveSequence;
+
+// function takes in array of numbers
+//      create new array that has numbers sorted
