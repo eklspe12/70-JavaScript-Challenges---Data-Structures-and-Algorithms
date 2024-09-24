849191eb639593bc1969ac226c15381f6f1f3a43
Spencer <eklund.spencer@outlook.com>
2024-09-24 10:41:40 -0600

    solved by converting both arrays to sets, iterating through second set to see if first set included any values - removing them if true or adding them if false, then converted to an array and returned array

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,22 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+	const results = new Set(arr1);
+	const set2 = new Set(arr2);
+	for (const value of set2) {
+		if (results.has(value)) {
+			results.delete(value);
+		} else {
+			results.add(value);
+		}
+	}
+	let resultArray = Array.from(results);
+	return resultArray;
+}
 
 module.exports = symmetricDifference;
+
+// function takes in two arrays
+//      Create sets for each array and an array to hold nonduplicates
+//      iterate through second set to see if any values are in arr1
+//          if so, delete from results array
+//          else add to results array
+// return results array
