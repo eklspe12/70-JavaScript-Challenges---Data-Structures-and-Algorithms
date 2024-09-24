ad3e5fce64d01c925156b3fffed9db6cd4bd8b3f
Spencer <eklund.spencer@outlook.com>
2024-09-24 11:02:43 -0600

    solved by creating a set to hold numbers, iterating through array to see if the target minus th ecurrent number was in the numberSet and returning the index of the paired number and the current number, returns an empty array if no pair exists

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,16 @@
-function twoSum() {}
+function twoSum(numbers, target) {
+	const numberSet = new Set();
+	for (i = 0; i < numbers.length; i++) {
+		let pair = target - numbers[i];
+		if (numberSet.has(pair)) {
+			return [numbers.indexOf(pair), i];
+		}
+		numberSet.add(numbers[i]);
+	}
+	return [];
+}
 
 module.exports = twoSum;
+
+// function takes in an array of integers
+//          create new set to hold used numbers
