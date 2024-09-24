32211fbacfc3719310296d094ce66bd8ed07cbe9
Spencer <eklund.spencer@outlook.com>
2024-09-24 11:30:08 -0600

    forgot to hit save

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -20,4 +20,12 @@ function longestConsecutiveSequence(numbers) {
 module.exports = longestConsecutiveSequence;
 
 // function takes in array of numbers
-//      create new array that has numbers sorted
+//      create new set from numbers
+//      create variable to hold longest sequence with a default of 0
+//          iterate through each number
+//          check to see if the previous number is not in set ensuring proper starting point
+//              if true set current number to number and currentLongest to 1
+//              run while loop to check for next numbers
+//                  currentLongest and currentNum ++ if true
+//              compare currentLongest to Longest
+//      return new longest
