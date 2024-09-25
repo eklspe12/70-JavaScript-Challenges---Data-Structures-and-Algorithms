68cb234677037c08dfe80e3f88d8427724737e8c
Spencer <eklund.spencer@outlook.com>
2024-09-25 14:26:05 -0600

    solved by iterating over str, adding each letter to stack, then running a while loop until the stack is empty - popping the top letter onto the string

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,24 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+	const stack = new Stack();
+	let reversed = '';
+	for (i = 0; i < str.length; i++) {
+		stack.push(str[i]);
+	}
+	while (!stack.isEmpty()) {
+		reversed = reversed + stack.pop();
+	}
+	return reversed;
+}
+
+console.log(reverseStringStack('hello'));
 
 module.exports = reverseStringStack;
+
+// function takes in a string to be reversed
+//      create new stack
+//      create variable to hold reversed string
+//      iterate over string, pushing each letter to stack
+//      iterate over stack, making reversed string equal to string + pop
+//      return reversedstring
