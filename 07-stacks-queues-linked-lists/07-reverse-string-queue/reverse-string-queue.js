b73c6dd62870860f9a594d75bf9d699f2d6367f8
Spencer <eklund.spencer@outlook.com>
2024-09-25 15:04:31 -0600

    solved by creating a new queue and a variable to hold reverse string, then iterated over string backwards - enqueueing each letter to the queue, then running a while loop until queue is empty - adding each letter to the reverse str before finally returning the string

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,20 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+	const queue = new Queue();
+	let reverse = '';
+	for (i = str.length - 1; i > -1; i--) {
+		queue.enqueue(str[i]);
+	}
+	while (!queue.isEmpty()) {
+		reverse = reverse + queue.dequeue();
+	}
+	return reverse;
+};
 
 module.exports = reverseStringWithQueue;
+
+// function takes in a string to be reversed (will need to be different since FIFO)
+//      create a queue
+//      iterate through str backwards enqueueing each character
+//      while loop to iterate queue until empty, dequeueing into string
