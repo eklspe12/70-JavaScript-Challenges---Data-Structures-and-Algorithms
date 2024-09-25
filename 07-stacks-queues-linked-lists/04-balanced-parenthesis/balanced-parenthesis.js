99bd3357743faddca23707f4fda3943272b7f3d7
Spencer <eklund.spencer@outlook.com>
2024-09-25 14:41:40 -0600

    iterated over string to see which parenthesis was present, pushing '(' and popping ')' while also checking check if the stack was empty before popping. if the stack was empty at a ')' or was not empty at the end of the string, returned false

--- 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
+++ 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
@@ -1,5 +1,34 @@
 const Stack = require('./stack');
 
-function balancedParenthesis() {}
+function balancedParenthesis(str) {
+	const stack = new Stack();
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === '(') {
+			stack.push(str[i]);
+		}
+		if (str[i] === ')') {
+			if (stack.isEmpty()) {
+				return false;
+			} else {
+				stack.pop();
+			}
+		}
+	}
+	if (stack.isEmpty()) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+console.log(balancedParenthesis('(()))'));
 
 module.exports = balancedParenthesis;
+
+// function takes in a str
+//      create a stack
+//      iterate over string
+//          if character is open, push
+//          if closed and stack size < -1 pop
+//              else return false
+//          return true
