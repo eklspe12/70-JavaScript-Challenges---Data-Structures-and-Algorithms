65aa0aa77237dac63b302eb507506a74925a7fd4
Spencer <eklund.spencer@outlook.com>
2024-09-25 10:54:18 -0600

    solved by iterating through words of array, then sorting them and seeing if they are in the hash table or not, adding them if they aren't and adding the word to the value array if they are, finally returning getValuea of the hashtable

--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,26 @@
 const HashTable = require('./HashTable');
 
-function anagramGrouping() {}
+function anagramGrouping(words) {
+	const anagrams = new HashTable();
+	for (const word of words) {
+		let sorted = word.split('').sort().join('');
+		if (!anagrams.has(sorted)) {
+			anagrams.set(sorted, [word]);
+		} else {
+			anagrams.get(sorted).push(word);
+		}
+	}
+	return anagrams.getValues();
+}
 
 module.exports = anagramGrouping;
+
+// function takes in string
+//      create an array to hold each word as a string without caps or punctuation
+//      create a hash table to hold values
+//          run for loop on array for each word
+//          if '' continue
+//          if sorted word is not in hashtable, add it and the original word as an array
+//          if sorted is in array, add original word to value array
+//      get values of hash tables
+//  return values of hashtable
