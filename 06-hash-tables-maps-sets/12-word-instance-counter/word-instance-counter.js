2101f0a49933e4ffe8b6e7c613fcccaffafc7ed1
Spencer <eklund.spencer@outlook.com>
2024-09-25 10:21:54 -0600

    solved by splitting string into an array of lower case words without punctuation, then iterated through array to check if hashtable had word, adding it if it didn't, or increasing the value by 1 if it did. finally checked to see if the target word was the current word before setting count to the current word value if true and then returning count

--- 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
+++ 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
@@ -1,5 +1,27 @@
 const HashTable = require('./HashTable');
 
-function wordInstanceCounter() {}
+function wordInstanceCounter(string, word) {
+	const wordHash = new HashTable();
+	const words = string.toLowerCase().split(/\W+/);
+	let count = 0;
+	const target = word;
+
+	for (const currentWord of words) {
+		if (currentWord === '') {
+			continue;
+		}
+		if (!wordHash.has(currentWord)) {
+			wordHash.set(currentWord, 1);
+		} else {
+			wordHash.set(currentWord, wordHash.get(currentWord) + 1);
+		}
+		if (target === currentWord) {
+			count = wordHash.get(currentWord);
+		}
+	}
+	return count;
+}
+
+console.log(wordInstanceCounter('The that The This The That', 'the'));
 
 module.exports = wordInstanceCounter;
