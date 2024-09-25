e379467c01904d2dfdbe71239bb99d9bd930449c
Spencer <eklund.spencer@outlook.com>
2024-09-25 15:15:04 -0600

    solved by pushing/enqueueing each letter into the stack/queue, then running a while loop to compare the stack and queue to see if characters matched eachother

--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,22 @@
 const Queue = require('./queue');
 const Stack = require('./stack');
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(str) {
+	const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+	const queue = new Queue();
+	const stack = new Stack();
+
+	for (i = 0; i < cleanStr.length; i++) {
+		stack.push(cleanStr[i]);
+		queue.enqueue(cleanStr[i]);
+	}
+
+	while (!queue.isEmpty()) {
+		if (stack.pop() !== queue.dequeue()) {
+			return false;
+		}
+	}
+	return true;
+}
 
 module.exports = isPalindromeQueueStack;
