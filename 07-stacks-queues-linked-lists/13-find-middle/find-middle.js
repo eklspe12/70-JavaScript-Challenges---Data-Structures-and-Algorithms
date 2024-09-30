4ce7cc88ba3a2d65664e317d71b1e9fe6962a9ec
Spencer <eklund.spencer@outlook.com>
2024-09-30 14:38:39 -0600

    solved by creating variables fast and slow both set to the list head, while fast !== null, fast = fast.next.next and slow = slow.next

--- 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
+++ 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
@@ -1,3 +1,15 @@
-function findMiddle() {}
+function findMiddle(LinkedList) {
+	let fast = LinkedList.head;
+	let slow = LinkedList.head;
+
+	while (fast !== null && fast.next !== null) {
+		fast = fast.next.next;
+		slow = slow.next;
+	}
+	return slow;
+}
 
 module.exports = findMiddle;
+
+//function takes in a linkedList to find middle
+//
