b00ad6900576b5ee38817c5eb5e4385b868d9109
Spencer <eklund.spencer@outlook.com>
2024-09-30 14:29:57 -0600

    solved by creating a list then iterating over the string backwords to add nodes to the list, then created a var called current that was set to list head, used a while loop to go through the list until current equaled zero, adding to the string each time

--- 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,5 +1,25 @@
 const { LinkedList } = require('./linked-list');
 
-function reverseStringLinkedList() {}
+function reverseStringLinkedList(str) {
+	const list = new LinkedList();
+	for (let i = str.length - 1; i >= 0; i--) {
+		list.add(str[i]);
+	}
+	let reversed = '';
+	let current = list.head;
+	while (current !== null) {
+		reversed += current.data;
+		current = current.next;
+	}
+	return reversed;
+}
+
+console.log(reverseStringLinkedList('hello'));
 
 module.exports = reverseStringLinkedList;
+
+// function takes in a string to be reversed
+//          initialize new linked list
+//          add each letter as node, for loop?
+//      iterate through list adding last letter on string and changing  node values
+// return string
