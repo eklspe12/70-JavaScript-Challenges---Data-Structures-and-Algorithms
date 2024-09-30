8b8bbf083deed001cdba8cd8b4e550b803cf0d1a
Spencer <eklund.spencer@outlook.com>
2024-09-30 15:06:52 -0600

    for each number finds difference between target and number, if linked list contains differnce it is returned with number, if not it is added to the list

--- 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
+++ 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
@@ -1,5 +1,15 @@
 const DoublyLinkedList = require('./DoublyLinkedList');
 
-function findPairSum() {}
+function findPairSum(nums, target) {
+	const used = new DoublyLinkedList();
+	for (const num of nums) {
+		difference = target - num;
+		if (used.contains(difference)) {
+			return [difference, num];
+		}
+		used.append(num);
+	}
+	return null;
+}
 
 module.exports = findPairSum;
