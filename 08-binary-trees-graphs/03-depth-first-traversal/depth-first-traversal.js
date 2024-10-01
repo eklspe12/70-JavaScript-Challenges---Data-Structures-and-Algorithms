8f5eb592ef87d6bad6ba6101c7b4dbbee2152270
Spencer <eklund.spencer@outlook.com>
2024-09-30 19:12:35 -0600

    depth first traversal function complete

--- 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
+++ 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
@@ -1,10 +1,37 @@
 const Stack = require('./stack');
 
-class Node {}
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.right = null;
+		this.left = null;
+	}
+}
+
+function depthFirstTraversal(root) {
+	if (!root) {
+		return [];
+	}
+	const result = [];
+	const stack = [];
+	stack.push(current.data);
+
+	while (stack.length > 0) {
+		const current = stack.pop();
+		result.push(current.data);
+	}
+	if (current.right) {
+		stack.push(current.right);
+	}
+	if (current.left) {
+		stack.push(current.left);
+	}
+	return result;
+}
 
 function depthFirstTraversal() {}
 
 module.exports = {
-  Node,
-  depthFirstTraversal,
+	Node,
+	depthFirstTraversal,
 };
