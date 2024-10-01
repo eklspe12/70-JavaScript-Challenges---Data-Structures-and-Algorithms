923d00938101a5495dc948abe6c87a51a473219e
Spencer <eklund.spencer@outlook.com>
2024-09-30 19:16:50 -0600

    solved by creating a recursive function to traverse left and right nodes until there is no data

--- 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
+++ 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
@@ -1,8 +1,32 @@
-class Node {}
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
 
-function recDepthFirstTraversal() {}
+const root = new Node('a');
+root.left = new Node('b');
+root.right = new Node('c');
+root.left.left = new Node('d');
+root.left.right = new Node('e');
+root.right.left = new Node('f');
+
+function recDepthFirstTraversal(root) {
+	const result = [];
+	function traverse(node) {
+		if (node !== null) {
+			result.push(node.data);
+			traverse(node.left);
+			traverse(node.right);
+		}
+	}
+	traverse(root);
+	return result;
+}
 
 module.exports = {
-  Node,
-  recDepthFirstTraversal,
+	Node,
+	recDepthFirstTraversal,
 };
