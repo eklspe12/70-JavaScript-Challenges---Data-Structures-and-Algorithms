9540dd63ee83f9e0a7c6ae8089573f2be93f7ec7
Spencer <eklund.spencer@outlook.com>
2024-10-01 11:15:13 -0600

    solved with a queue instead of a stack

--- 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
+++ 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
@@ -1,5 +1,25 @@
 const Queue = require('./queue');
 
-function breadthFirstTraversal() {}
+function breadthFirstTraversal(graph, start) {
+	const visited = new Set();
+	const result = [];
+	const queue = new Queue();
+
+	queue.enqueue(start);
+	visited.add(start);
+
+	while (!queue.isEmpty()) {
+		const currentVertex = queue.dequeue();
+		result.push(currentVertex);
+
+		for (const n of graph.adjacencyList[currentVertex]) {
+			if (!visited.has(n)) {
+				queue.enqueue(n);
+				visited.add(n);
+			}
+		}
+	}
+	return result;
+}
 
 module.exports = breadthFirstTraversal;
