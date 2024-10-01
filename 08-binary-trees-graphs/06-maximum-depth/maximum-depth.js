378cc9ae87206f9d6b6d985ea1aa27b9c4fd80ca
Spencer <eklund.spencer@outlook.com>
2024-09-30 19:29:29 -0600

    solved by recursively traversing left and right roots, adding 1 for each layer

--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
@@ -1,8 +1,24 @@
-class Node {}
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
 
-function maxDepth() {}
+function maxDepth(root) {
+	if (!root) {
+		return 0;
+	}
+	let left = maxDepth(root.left);
+	let right = maxDepth(root.right);
+
+	return Math.max(left, right) + 1;
+}
 
 module.exports = {
-  maxDepth,
-  Node,
+	maxDepth,
+	Node,
 };
+
+// function takes in a root
