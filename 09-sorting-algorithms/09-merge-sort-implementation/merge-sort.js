063fa899df4fde1dfb2020fdc6ff3cecc90ad023
Spencer <eklund.spencer@outlook.com>
2024-10-02 13:23:21 -0600

    merge sort created

--- 09-sorting-algorithms/09-merge-sort-implementation/merge-sort.js
+++ 09-sorting-algorithms/09-merge-sort-implementation/merge-sort.js
@@ -1,3 +1,31 @@
-function mergeSort() {}
+function mergeSort(arr) {
+	if (arr.length <= 1) {
+		return arr;
+	}
+	const mid = Math.floor(arr.length / 2);
+	const left = mergeSort(arr.slice(0, mid));
+	const right = mergeSort(arr.slice(mid));
+
+	return merge(left, right);
+}
+
+function merge(left, right) {
+	const merged = [];
+	let indexLeft = 0;
+	let indexRight = 0;
+
+	while (indexLeft < left.length && indexRight < right.length) {
+		if (left[indexLeft] < right[indexRight]) {
+			merged.push(left[indexLeft]);
+			indexLeft++;
+		} else {
+			merged.push(right[indexRight]);
+			indexRight++;
+		}
+	}
+	return merged.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+}
+
+console.log(mergeSort([5, 2, 7, 3, 1, 6, 4, 8]));
 
 module.exports = mergeSort;
