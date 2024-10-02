6c048360824d1f05145c6031acc75f084048dc3e
Spencer <eklund.spencer@outlook.com>
2024-10-02 13:05:35 -0600

    selection sort implemented

--- 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
+++ 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
@@ -1,3 +1,19 @@
-function selectionSort() {}
+function selectionSort(arr) {
+	for (let i = 0; i < arr.length - 1; i++) {
+		let min = i;
+
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+		if (min !== i) {
+			[arr[i], arr[min]] = [arr[min], arr[i]];
+		}
+	}
+	return arr;
+}
+
+console.log(selectionSort([4, 2, 6, 7, 1]));
 
 module.exports = selectionSort;
