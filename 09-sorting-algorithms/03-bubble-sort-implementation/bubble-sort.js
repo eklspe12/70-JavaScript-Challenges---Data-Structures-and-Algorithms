72081e17db5b38c850e6847e971d98233422d276
Spencer <eklund.spencer@outlook.com>
2024-10-02 12:31:17 -0600

    solved by running two for loops to compare the values of each consecutive position, swapping them to be ordered until the highest number is swapped all the way to the end of the array

--- 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
+++ 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				const swap = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = swap;
+			}
+		}
+	}
+	return arr;
+}
 
+console.log(bubbleSort([1, 4, 2, 45, 5, 7, 3, 67]));
 module.exports = bubbleSort;
