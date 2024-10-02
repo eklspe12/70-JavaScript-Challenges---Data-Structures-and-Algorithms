16dc4adf21c7d4ae305507d1678492debceace92
Spencer <eklund.spencer@outlook.com>
2024-10-02 13:32:40 -0600

    quick sort implemented

--- 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
+++ 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
@@ -1,5 +1,22 @@
-function quickSort() {
+function quickSort(arr) {
+	if (arr.length <= 1) {
+		return arr;
+	}
+	const pivot = arr[arr.length - 1];
+	const left = [];
+	const right = [];
+	console.log(left, pivot, right);
 
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (arr[i] < pivot) {
+			left.push(arr[i]);
+		} else {
+			right.push(arr[i]);
+		}
+	}
+	return [...quickSort(left), pivot, ...quickSort(right)];
 }
+
+console.log(quickSort([3, 1, 7, 4, 9, 12]));
 
 module.exports = quickSort;
