75371442c1286544d3e3fb217bed2f6acbea959a
Spencer <eklund.spencer@outlook.com>
2024-10-09 16:13:37 -0600

    did the opposite last time

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-practice.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-practice.js
@@ -1,7 +1,12 @@
 function symmetricDifference(arr1, arr2) {
-	const set = new Set(arr1);
+	const set = new Set();
+	for (const item of arr1) {
+		if (!arr2.includes(item)) {
+			set.add(item);
+		}
+	}
 	for (const item of arr2) {
-		if (!set.has(item)) {
+		if (!arr1.includes(item)) {
 			set.add(item);
 		}
 	}
